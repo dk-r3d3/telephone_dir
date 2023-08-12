31ddcce1aba33596ba756594c34191196542f345
r3_d3 <93869150+dk-r3d3@users.noreply.github.com>
2023-08-12 14:34:25 +0700

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-ding='UTF-8') as f:
-        #     lines = f.readlines()
-        #     data = {}
-        #     for i in range(1, len(lines)):
-        #         data[i] = lines[i]
-        #         print(f"{i}.", lines[i])
